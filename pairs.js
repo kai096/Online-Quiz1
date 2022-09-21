782e02bd5af29f54cdf293030a8bec3f6a25d787
kai096 <108310585+kai096@users.noreply.github.com>
2022-09-21 15:18:47 +1000

    pairs game

---
+++ pairs.js
@@ -0,0 +1,88 @@
+const cards = document.querySelectorAll('.card');
+const matchedText = document.getElementById('matched');
+const btn = document.getElementById('btn');
+const countDown = document.getElementById('countDown');
+let cardOne, cardTwo; //falsy value
+let matched = 0;
+let time = 120;
+
+var count = setInterval(timer, 1000)
+function timer(){
+    time = time-1
+    if(count <= 0){
+        clearInterval(count);
+        location.reload();
+    }
+    countDown.innerText = time
+}
+
+function turnOver(e){
+    //console.log(e.target);
+    clickedCard = e.target;
+    if (cardOne !== clickedCard) {
+        clickedCard.classList.add('flip')
+        if(!cardOne) {
+            return cardOne = clickedCard
+        }
+    
+        cardTwo = clickedCard
+        //console.log(cardOne, cardTwo);
+        let cardOneImg = cardOne.querySelector('.back-view img').src;
+        let cardTwoImg = cardTwo.querySelector('.back-view img').src;
+        //console.log(cardOneImg, cardTwoImg)
+        matchedSet(cardOneImg, cardTwoImg)
+    }
+}
+function matchedSet(img1, img2){
+    console.log(img1, img2)
+    if (img1 === img2) {
+       // console.log(matched)
+        matched++
+        matchedText.innerText = matched;
+        if (matched == 8) {
+            matched = 0
+            setTimeout(() => {
+                return shuffleCard()
+                
+            }, 1000);
+        }
+        cardOne.removeEventListener('click', turnOver)
+        cardTwo.removeEventListener('click', turnOver)
+        cardOne = cardTwo = '';
+    }
+        //else
+        else{
+             setTimeout(() => {
+                cardOne.classList.add('shake')
+                cardTwo.classList.add('shake')
+            }, 400);
+            setTimeout(() => {
+                cardOne.classList.remove("shake", "flip");
+                cardTwo.classList.remove("shake", "flip");
+                cardOne = cardTwo = ''
+            }, 1000);
+        }
+           
+        
+}
+
+function shuffleCard() {
+    matched = 0;
+    timer()
+    console.log('shuffling')
+    
+    let imgI = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8,]
+    imgI = imgI.sort(() => Math.random() > 0.5 ? 1 : -1);
+    //console.log(imgI)
+    cards.forEach((card, i) => {
+        card.classList.remove('flip')
+        let imgTag = card.querySelector('.back-view img');
+        imgTag.src = `./pairs.images/${imgI[i]}.png`
+        console.log(imgTag)
+        card.addEventListener('click', turnOver)
+    })
+}
+cards.forEach(card => {
+    //console.log(card)
+    card.addEventListener('click', turnOver)
+})
